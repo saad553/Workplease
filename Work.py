814c610d1537dba0eddee4036bd3697b62024c14
saad553 <62612217+saad553@users.noreply.github.com>
2024-01-10 13:26:17 +0100

    Added hello instead

--- Work.py
+++ Work.py
@@ -8,5 +8,5 @@ age = int(age)
 # Print a message using the user's information
 print(f"Hello, {name}! You are {age} years old.")
 print(f"Hello, {name}! You are {age} years old.")
-print(f"Hy, {name}! You are {age} years old.")
+print(f"Hello, {name}! You are {age} years old.")
 
